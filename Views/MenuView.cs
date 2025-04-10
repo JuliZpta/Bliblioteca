dc56067e967164c3e2b64ae4af14780915b42981
JuliZpta <zapatajulian42@gmail.com>
2025-04-10 12:33:28 -0500

    .

--- Views/MenuView.cs
+++ Views/MenuView.cs
@@ -2,7 +2,7 @@ namespace BibliotecaMVC.Views {
     public static class MenuView {
         public static void MostrarMenuPrincipal() {
             Console.WriteLine("\n=== Menú Biblioteca ===");
-            Console.WriteLine("1. Registrar material");
+            Console.WriteLine("1. Registrar materiall");
             Console.WriteLine("2. Registrar persona");
             Console.WriteLine("3. Eliminar persona");
             Console.WriteLine("4. Registrar préstamo");
